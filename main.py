7bf53723bf2d46654fc391bf2785c2ceccd3edc1
EuiseongHong <euiseong.hong@gmail.com>
2024-04-11 10:45:02 +0000

    main.py

--- main.py
+++ main.py
@@ -14,7 +14,3 @@ def twoSum(nums: List[int], target: int) -> List[int]:
                 answer[1] = k
                 return answer
     return answer
-
-nums = [2,7,11,15]
-target = 9
-print(twoSum(nums, target))
